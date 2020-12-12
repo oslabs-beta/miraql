2f9c7e0ab679bd151c2e8d4adfca313e00856f21
Miguel Manalo <miguelmanalo@protonmail.com>
2020-12-12 15:49:01 -0500

    moved grid in new submit query component

--- client/components/Container.jsx
+++ client/components/Container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, GridItem } from '@chakra-ui/react';
 import SubmitUrlComponent from './SubmitUrlComponent.jsx';
+import SubmitQuery from './SubmitQuery.jsx';
 
 function Container() {
   return (
@@ -13,22 +14,8 @@ function Container() {
           <SubmitUrlComponent />
         </GridItem>
       </Grid>
-      <Grid
-        h="100%"
-        border="1px"
-        borderColor="gray.200"
-        autoFlow="column dense"
-      >
-        <GridItem bg="#F7FAFC" colSpan={1}>
-          Submit Query
-        </GridItem>
-        <GridItem bg="#EDF2F7" colSpan={1}>
-          Response
-        </GridItem>
-        <GridItem bg="#E2E8F0" colSpan={2}>
-          Submit Schema
-        </GridItem>
-      </Grid>
+      <SubmitQuery />
+   
     </div>
   );
 }
