5608260fc81a7586feb9a58baecbeb5ee7643f44
Sanaya Mirpuri <sanayamirpuri@Sanayas-MBP.AN-810-AP-I-AC>
2020-12-14 12:08:18 -0500

    first iteration of submitQuery

--- client/components/Container.jsx
+++ client/components/Container.jsx
@@ -3,13 +3,14 @@ import { Grid, GridItem } from '@chakra-ui/react';
 import SubmitUrlComponent from './SubmitUrlComponent.jsx';
 import SubmitQuery from './SubmitQuery.jsx';
 
+// container holds the MiraQL header and submitURL component
 function Container() {
   return (
     <div>
       <Grid borderTop="1px" borderColor="gray.200" h={12}>
         <GridItem bg="red.50">MiraQL</GridItem>
       </Grid>
-          <SubmitUrlComponent />
+      <SubmitUrlComponent />
     </div>
   );
 }
