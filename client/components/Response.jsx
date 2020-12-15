d834b8f54d5570febd2af4006503011a805d86d8
Sanaya Mirpuri <sanayamirpuri@Sanayas-MBP.AN-810-AP-I-AC>
2020-12-14 20:04:43 -0500

    submitQuery with response in response component and logo

--- client/components/Response.jsx
+++ client/components/Response.jsx
@@ -1,1 +1,16 @@
 import React from 'react';
+
+function Response({ urlValue, query, fetchResponse }) {
+  // let unstringifyResponse = JSON.parse(fetchResponse);
+
+  return (
+    <div>
+      <p>Response bitch</p>
+      {/* <p>{urlValue}</p>
+      <p>{query}</p> */}
+      <p>{fetchResponse}</p>
+    </div>
+  );
+}
+
+export default Response;
