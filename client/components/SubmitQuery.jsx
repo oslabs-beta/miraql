fe6d5a3fdaf2ecc44052e8acac9c5aaea51478ae
Sanaya Mirpuri <sanayamirpuri@Sanayas-MBP.AN-810-AP-I-AC>
2020-12-16 19:14:49 -0500

    first iteration of errors with testing codemirror resizing

--- client/components/SubmitQuery.jsx
+++ client/components/SubmitQuery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Grid,
   GridItem,
@@ -19,13 +19,13 @@ import '../../node_modules/codemirror/lib/codemirror.css';
 import '../../node_modules/codemirror/theme/neo.css';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/addon/edit/closebrackets';
-import Schema from './Schema.jsx'
+import Schema from './Schema.jsx';
 
 function SubmitQuery({ urlValue }) {
   // react hooks to hold query in state
-  let [query, setQuery] = useState("");
+  let [query, setQuery] = useState('');
   /// react hooks to hold fetch response in state
-  let [fetchResponse, setFetchResponse] = useState("");
+  let [fetchResponse, setFetchResponse] = useState('');
 
   // handle query text input change
   const handleQueryChange = (e) => {
@@ -37,9 +37,9 @@ function SubmitQuery({ urlValue }) {
   const getQueryResponse = () => {
     // execute a fetch request
     fetch(`${urlValue}`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-type": "application/json",
+        'Content-type': 'application/json',
       },
       body: JSON.stringify({ query: query }),
     })
@@ -51,7 +51,9 @@ function SubmitQuery({ urlValue }) {
 
   return (
     <Grid h="100%" border="1px" borderColor="gray.200" autoFlow="column dense">
-      <GridItem bg="#F7FAFC" colSpan={1}>
+      <GridItem bg="#F7FAFC" colStart={1} colEnd={1}>
+        {/* <Grid templateColumns="repeat(5, 1fr)">
+      <GridItem bg="#F7FAFC"> */}
         <Tabs variant="enclosed" colorScheme="pink">
           <TabList>
             <Tab>Submit Query</Tab>
@@ -60,6 +62,9 @@ function SubmitQuery({ urlValue }) {
             <TabPanel>
               <Stack direction={'column'}>
                 <CodeMirror
+                  // editorDidMount={(editor) => {
+                  //   editor.setSize(200, 500);
+                  // }}
                   value={query}
                   options={{
                     autoCloseBrackets: true,
@@ -72,6 +77,7 @@ function SubmitQuery({ urlValue }) {
                     setQuery(value);
                   }}
                   onChange={(editor, data, value) => {}}
+                  max-width="100%"
                 />
                 <Button colorScheme="pink" size="sm" onClick={getQueryResponse}>
                   Submit
@@ -82,6 +88,7 @@ function SubmitQuery({ urlValue }) {
         </Tabs>
       </GridItem>
       <GridItem bg="#EDF2F7" colSpan={1}>
+        {/* <GridItem bg="#EDF2F7"> */}
         <Response
           urlValue={urlValue}
           query={query}
@@ -89,6 +96,7 @@ function SubmitQuery({ urlValue }) {
         />
       </GridItem>
       <GridItem bg="#E2E8F0" colSpan={2}>
+        {/* <GridItem bg="#EDF2F7"> */}
         <Schema />
       </GridItem>
     </Grid>
