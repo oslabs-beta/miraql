b917f44f23b0783083bf92016bcb5c17012b8cf8
Kristiina Eelnurme <kristiina.eelnurme@gmail.com>
2020-12-16 20:47:35 -0500

    Fixed some form formatting but still needs more work

--- client/components/DatabInputModal.jsx
+++ client/components/DatabInputModal.jsx
@@ -17,7 +17,9 @@ import {
   FormControl,
   FormLabel,
   HStack,
-  StackDivider,
+  VStack,
+  Flex,
+  Spacer,
 } from '@chakra-ui/react';
 
 function DatabInputModal() {
@@ -32,31 +34,26 @@ function DatabInputModal() {
           <ModalCloseButton />
           <ModalHeader>
               {/* this first editable block is an input field for the table name */}
-              <Editable defaultValue="Table Name">
+              <Editable border="2px" borderColor="gray.200" borderRadius="10px" defaultValue="Table Name">
               <EditablePreview />
               <EditableInput />
             </Editable>
           </ModalHeader>
+          <Flex>
           <ModalBody>
-          {/* <HStack spacing="24px">
-            <h2>Add Field Name</h2>
-            <h2>Default Value</h2>
-            <h2>Primary Key</h2>
-            <h2>Unique</h2>
-            <h2>Required?</h2>
-            <h2>Queryable?</h2>
-            <h2>Table Relationship</h2>
-            <h2>Field Relationship</h2>
-            <h2>Type of Relationship</h2>
-          </HStack> */}
-          <br></br>
           <HStack spacing="24px">
             {/* this second editable block is an input field for the field name */}
-            <Editable defaultValue="field name">
-              <EditablePreview />
-              <EditableInput />
+              <VStack spacing="8px">
+            <p>Field name</p>
+            <Editable border="2px" borderColor="gray.200" borderRadius="10px" defaultValue="enter name">
+                <EditablePreview />
+                <EditableInput />
             </Editable>
+              </VStack>
+              <Spacer />
             {/* Dropdown menu to select your field type */}
+            <VStack spacing="8px">
+            <p>Field Type</p>
             <Select placeholder="–">
               <option value="id">ID</option>
               <option value="string">String</option>
@@ -64,48 +61,79 @@ function DatabInputModal() {
               <option value="int">Int</option>
               <option value="float">Float</option>
             </Select>
+            </VStack>
+            <Spacer />
             {/* this  editable block is an input field for the default value */}
-            <Editable defaultValue="Default Value">
+            <VStack spacing="8px">
+            <p>Default Value</p>
+            <Editable border="2px" borderColor="gray.200" borderRadius="10px" defaultValue="enter value">
               <EditablePreview />
               <EditableInput />
             </Editable>
+            </VStack>
+            <Spacer />
             {/* these blocks are for our four switches */}
             <FormControl id="primary-key" display="flex" alignItems="center">
-              <FormLabel htmlFor="primary-key" mb="0">
-                {/* Primary Key */}
-              </FormLabel>
-              <Switch id="primary-key-switch" />
+              <VStack spacing="8px">
+                <FormLabel htmlFor="primary-key" mb="0">
+                  Primary Key
+                </FormLabel>
+                <Switch id="primary-key-switch" />
+              </VStack>
             </FormControl>
+            <Spacer />
             <FormControl id="unique" display="flex" alignItems="center">
-              <FormLabel htmlFor="unique" mb="0">
-                {/* Unique */}
-              </FormLabel>
-              <Switch id="unique-switch" />
+              <VStack spacing="8px">
+                <FormLabel htmlFor="unique" mb="0">
+                  Unique
+                </FormLabel>
+                <Switch id="unique-switch" />
+              </VStack>
             </FormControl>
+            <Spacer />
             <FormControl id="required" display="flex" alignItems="center">
-              <FormLabel htmlFor="Required" mb="0">
-                {/* Required? */}
-              </FormLabel>
-              <Switch id="required-switch" />
+              <VStack spacing="8px">
+                <FormLabel htmlFor="Required" mb="0">
+                  Required?
+                </FormLabel>
+                <Switch id="required-switch" />]
+              </VStack>
             </FormControl>
+            <Spacer />
             <FormControl id="queryable" display="flex" alignItems="center">
-              <FormLabel htmlFor="queryable" mb="0">
-                {/* Queryable? */}
-              </FormLabel>
-              <Switch id="queryable-switch" />
+              <VStack spacing="8px">
+                <FormLabel htmlFor="queryable" mb="0">
+                  Queryable?
+                </FormLabel>
+                <Switch id="queryable-switch" />
+              </VStack>
             </FormControl>
+            <Spacer />
             {/* these three selects are for table relationship, field relationship, and type of relationship */}
-            <Select placeholder="Table Relationship">
-              <option value="id">-</option>
+            <VStack spacing="8px">
+              <p>Table Relationship</p>
+            <Select placeholder="-">
+              <option value="id">ID</option>
             </Select>
-            <Select placeholder="Field Relationship">
-              <option value="id">-</option>
+            </VStack>
+            <Spacer />
+            <VStack spacing="8px">
+              <p>Field Relationship</p>
+            <Select placeholder="-">
+              <option value="id">ID</option>
             </Select>
-            <Select placeholder="Type of Relationship">
-              <option value="id">-</option>
+            </VStack>
+            <Spacer />
+            <VStack spacing="8px">
+              <p>Type of Relationship</p>
+            <Select placeholder="-">
+              <option value="id">ID</option>
             </Select>
+            </VStack>
+            <Spacer />
             </HStack>
-          </ModalBody>
+            </ModalBody>
+          </Flex>
 
           <ModalFooter>
             <Button variant="ghost">+ Add Field</Button>
