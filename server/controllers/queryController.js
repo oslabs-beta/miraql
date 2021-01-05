1d7706928c1f75e4f5aadfc19fa60294ef55d4ff
Miguel Manalo <miguelmanalo@protonmail.com>
2021-01-05 15:39:45 -0500

    safety commit: working on route handlers and error handlers in server.js, Kristiina and sql queries Miguel

--- server/controllers/queryController.js
+++ server/controllers/queryController.js
@@ -2,21 +2,111 @@ const db = require('../models/queryModels');
 
 const queryController = {};
 
-// should create a new row in the user_tables table
+// POST request: should create a new row in the user_tables table
   // req.body would send the 'table_name'
+queryController.addRowUserTable = (req, res, next) => {
+  const { table_name } = req.body;
+  const text = `INSERT INTO user_tables (table_name) VALUES ($1)`;
 
-// input multiple rows in the 
-// grabbing all the fields from the req.body
-// query would be like: INSERT INTO table_name (col, col, col etc) VALUES ($1, $2, $3 etc
-// 
+  db.query(text, [table_name])
+    .then((data) => {
+      res.status(200).send(`table_name added to user_tables table with ID: ${data.insertId}`);
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-// should create a new item in the database
+// user adds many rows to a table - 'post' request
+queryController.createSchemaTable = (req, res, next) => {
+  const arr = req.body;
+  // we will get an array of objects with multiple rows that will need to be added
+  let addToTable = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    const userFieldName = arr[i].fieldName
+    const userFieldType = arr[i].fieldType;
+    const userDefaultValue = arr[i].defaultValue;
+    const userPrimaryKey = arr[i].primaryKey;
+    const userUnique = arr[i].unique;
+    const userRequired = arr[i].required;
+    const userQueryable = arr[i].queryable;
+    const userTableRelate = arr[i].tableRelationship;
+    const userFieldRelate = arr[i].fieldRelationship;
+    const userTypeRelate = arr[i].typeRelationship;
+    console.log(
+      'userFieldName: ',
+      userFieldName,
+      'userFieldType: ',
+      userFieldType,
+      'userDefaultValue:',
+      userDefaultValue
+    );
+
+    let concatStr =
+      `${userFieldName}` +
+      ',' +
+      `${userFieldType}` +
+      ',' +
+      `${userDefaultValue}` +
+      ',' +
+      `${userPrimaryKey}` +
+      ',' +
+      `${userUnique}` +
+      ',' +
+      `${userRequired}` +
+      ',' +
+      `${userQueryable}` +
+      ',' +
+      `${userTableRelate}` +
+      ',' +
+      `${userFieldRelate}` +
+      ',' +
+      `${userTypeRelate}` +
+      '),';
+    console.log('this is the concatenated string', concatStr);
+    addToTable = addToTable + concatStr;
+    console.log('this is our addToTable query after concat:', addToTable);
+  }
+  let oldDbQuery = `INSERT INTO user_tables (table_name) VALUES ($1) `;
+
+  let newDB = oldDbQuery + addCart;
+  // console.log(newDB, 'newDB');
+  let lengthSlice = newDB.length - 1;
+  // console.log(lengthSlice, 'lengthSlice');
+  let newDbQuery = newDB.slice(0, lengthSlice);
+  // console.log(newDbQuery, 'newDbQuery');
+  // console.log('this is the new DB query, pls work! ', newDbQuery);
+
+  db.query(newDbQuery)
+    .then((data) => {
+      // console.log(`this is the data from the user's CART: `, data.rows);
+      res.locals.userCart = data.rows;
+    })
+    .then(next)
+    .catch(() => {
+      // next(err)
+      next({
+        log: `cartController.getProductsUserCart: ERROR: Error putting the user's cart in the database`,
+        message: {
+          err:
+            'Error occurred in cartController.getProductsUserCart. Check server logs for more details.',
+        },
+      });
+    });
+};
+
+// POST request: input all rows into fields table (user-created instance of a schema table) 
+  // grabbing all the fields from the req.body
+  // query would be like: INSERT INTO table_name (col, col, col etc) VALUES ($1, $2, $3 etc
+  // do what Jenny did (see slack msg)
 queryController.postTask = (req, res, next) => {
   const { item } = req.body;
   const text = `INSERT INTO Tasks (item) VALUES ($1)`;
 
   // request sent from server to db (like fetch), passing in our query
-  pool.query(text, [item])
+  db.query(text, [item])
     .then((data) => {
       res.status(200).send(`Task added with ID: ${data.insertId}`);
       next();
@@ -26,13 +116,18 @@ queryController.postTask = (req, res, next) => {
     });
 };
 
-// should create a new item in the database
-queryController.postTask = (req, res, next) => {
+// POST request: to add a new row in an existing fields/schema table
+queryController.addRowSchemaTable = (req, res, next) => {
+
+};
+
+// PATCH request middleware: edit/update existing fields in an existing fields/schema table
+queryController.editRowSchemaTable = (req, res, next) => {
   const { item } = req.body;
   const text = `INSERT INTO Tasks (item) VALUES ($1)`;
 
   // request sent from server to db (like fetch), passing in our query
-  pool.query(text, [item])
+  db.query(text, [item])
     .then((data) => {
       res.status(200).send(`Task added with ID: ${data.insertId}`);
       next();
@@ -42,14 +137,15 @@ queryController.postTask = (req, res, next) => {
     });
 };
 
-// should find items in the database based on an ID number and delete that item if it exists
-queryController.deleteTask = (req, res, next) => {
+// DELETE request: to delete a row from a schema table
+  // if deleting entire table, need to also delete row from user_tables table
+queryController.deleteRowSchemaTable = (req, res, next) => {
   // query Task table based on id and delete if item exists
   const id = parseInt(req.params.id);
   const text = `DELETE FROM Tasks WHERE id = $1`;
 
   // send our query over to the db passing in the query and id
-  pool.query(text, [id])
+  db.query(text, [id])
     .then(() => {
       res.status(200).send(`Task deleted with ID: ${id}`);
       next();
@@ -58,3 +154,11 @@ queryController.deleteTask = (req, res, next) => {
       console.log(err);
     });
 };
+
+// DELETE request: to delete an entire schema table
+  // if deleting entire table, need to also delete row from user_tables table
+queryController.deleteSchemaTable = (req, res, next) => {
+
+};
+
+module.exports = queryController;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const queryController = require('./controllers/queryController.js');
 
-
+// handle parsing request body and cookies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
 app.use('/build', express.static(path.join(__dirname, '../build')));
@@ -12,6 +16,23 @@ app.get('/', (req, res) => {
 });
 }
 
+// make endpoint routes for our CRUD middleware
+app.post('/usertable', queryController.addRowUserTable);
+app.post('/schemas', queryController.createSchemaTable);
+app.post('/schemas', queryController.addRowSchemaTable);
+app.patch('/schemas', queryController.editRowSchemaTable);
+app.delete('/schemas', queryController.deleteRowSchemaTable);
+app.delete('/schemas', queryController.deleteSchemaTable);
+
+
+// catch-all route handler for any requests to an unknown route
+app.use('*', (req, res) => res.sendStatus(404));
+
+// global error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send('Internal Server Error');
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!')
