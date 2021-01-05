8ba50e14f1b119aa07a629e89d5ed7544848303a
Sabrina <s.goldfarb2@gmail.com>
2021-01-05 17:37:31 -0600

    created cards to save table data in

--- client/components/SchemaCards.jsx
+++ client/components/SchemaCards.jsx
@@ -4,34 +4,35 @@ import { Table, Thead, Tbody, Tr, Th, Td, TableCaption } from "@chakra-ui/react"
 const SchemaCards = ({ query, urlValue, fetchResponse }) => {
   return (
     <>
-      <p>SCHEMA CARDS BIATCH</p>
-      <Table variant="striped">
- 
-  <Thead>
-    <Tr>
-      <Th>To convert</Th>
-      <Th>into</Th>
-      <Th isNumeric>multiply by</Th>
-    </Tr>
-  </Thead>
-  <Tbody>
-    <Tr>
-      <Td>inches</Td>
-      <Td>millimetres (mm)</Td>
-      <Td isNumeric>25.4</Td>
-    </Tr>
-    <Tr>
-      <Td>feet</Td>
-      <Td>centimetres (cm)</Td>
-      <Td isNumeric>30.48</Td>
-    </Tr>
-    <Tr>
-      <Td>yards</Td>
-      <Td>metres (m)</Td>
-      <Td isNumeric>0.91444</Td>
-    </Tr>
-  </Tbody>
-</Table>
+    <br></br>
+    <br></br>
+    <h2>Books</h2>
+          <Table variant="striped">
+      <Thead>
+        <Tr>
+          <Th>Field Name</Th>
+          <Th>Type</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        <Tr>
+          <Td>ID</Td>
+          <Td>Id</Td>
+        </Tr>
+        <Tr>
+          <Td>Name</Td>
+          <Td>String</Td>
+        </Tr>
+        <Tr>
+          <Td>Author</Td>
+          <Td>String</Td>
+        </Tr>
+        <Tr>
+          <Td>Genre</Td>
+          <Td>String</Td>
+        </Tr>
+      </Tbody>
+    </Table>
 
     </>
   );
