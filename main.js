3b997fd6aee3531f4f4181dedf31916296f20fa5
Miguel Manalo <miguelmanalo@protonmail.com>
2021-01-07 00:45:44 -0500

    added electron dev packages electron log and electron context menu

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const log = require('electron-log');
+const contextMenu = require('electron-context-menu');
+
+contextMenu({
+	prepend: (defaultActions, params, browserWindow) => [
+		{
+			label: 'Rainbow',
+			// Only show it when right-clicking images
+			visible: params.mediaType === 'image'
+		},
+		{
+			label: 'Search Google for “{selection}”',
+			// Only show it when right-clicking text
+			visible: params.selectionText.trim().length > 0,
+			click: () => {
+				shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
+			}
+		}
+	]
+});
+
+let mainWindow;
+(async () => {
+	await app.whenReady();
+
+	mainWindow = new BrowserWindow(
+		webPreferences, {
+			spellcheck: true
+    }
+	);
+})();
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,8 +38,7 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-    },
-    icon: path.join(__dirname, 'assets/icons/png/1024x1024.png'),
+    }
   });
 
   win.loadFile('./index.html');
@@ -32,3 +62,4 @@ app.on(
   'window-all-closed',
   () => process.platform !== 'darwin' && app.quit() // "darwin" targets macOS only.
 );
+
